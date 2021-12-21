d493c41ad9a04620a834f932f26f15c60417e3c9
darkmonger <armi011@hotmail.com>
2021-12-21 15:56:12 +0100

    first version

---
+++ dogslib/dogslib/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace dogs
+{
+    
+    
+        public abstract class Animal
+        {
+            
+            public abstract string bark();
+        }
+
+        public class Dog : Animal
+        {
+
+
+            private string name;
+
+            public  string getname()
+            {
+                return this.name;
+            }
+            public Dog() { }
+
+            public Dog(string nm)
+            {
+                this.name = nm;
+            }
+
+            public override string bark()
+            {
+                return ("bow-wow");
+            }
+
+
+         
+        }
+        public class SheepDog : Dog
+        {
+            private int age;
+             
+            public SheepDog () {}
+            public SheepDog(string nm, int age) : base(nm)
+            {
+                this.age = age;
+            
+            }
+            
+            
+
+            public override string bark()
+            {
+                Random random = new Random();
+                int min = 1;
+                int max = 10;
+                string barking = "";
+                int number = random.Next(min, max);
+                for (int i = 0; i < number; i++)
+                {
+                    barking += "bow-wow ";
+                }
+                return ("barking");
+            }
+        }
+        
+        public int getage()
+        {
+           return this.getage;
+        }
+    
+}
+
---
+++ dogsmain/dogsmain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using dogs;
+
+namespace dogsmain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            Console.WriteLine(dog1.bark());
+            Console.ReadLine();
+
+
+            SheepDog dog2 = new SheepDog("rufus");
+            Console.WriteLine(dog2.bark());
+
+            SheepDog dog3 = new SheepDog();
+            Console.WriteLine(dog2.bark()); 
+
+            SheepDog dog4 = new SheepDog();
+            Console.WriteLine(dog2.bark());
+
+            SheepDog dog5 = new SheepDog();
+            Console.WriteLine(dog2.bark());
+
+            
+        }
+    }
+}
